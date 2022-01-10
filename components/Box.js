cd2ab49ced60f4e807155d24dc0d937019caef89
Carlos Gonzalez <cyberpolin@gmail.com>
2022-01-09 22:45:36 -0600

    very basic index list

---
+++ components/Box.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Box.module.sass'
+
+const Box = ({title, children, bgColor}) => (
+  <div className={styles.container}>
+    <h2>{title}</h2>
+    <>{children}</>
+  </div>
+)
+
+export default Box
