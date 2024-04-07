da3ecb7afa21ee14579e88032949b5e33698dfe5
(cyberpolin) = 'Carlos González <cyberpolin@gmail.com>
2024-04-07 07:08:31 -0600

    comply with next

--- pages/index.js
+++ pages/index.js
@@ -129,16 +129,16 @@ export default function Home(props) {
               />
               <div className={`flex items-center justify-center  `}>
                 {data.youtube?.[0] ? (
-                  // eslint-disable-next-line
                   <iframe
+                    // eslint-disable-next-line
                     width="100%"
                     height="315"
                     src={data.youtube?.[0]}
                     title="YouTube video player"
                     frameorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerpolicy="strict-origin-when-cross-origin"
-                    allowfullscreen
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
                   ></iframe>
                 ) : (
                   <img
